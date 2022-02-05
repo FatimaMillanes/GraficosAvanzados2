ff6ab109aaae6fe0f40989a67e8e85818eb775c0
fatim <fatima.rosela@hotmail.com>
2022-02-05 11:27:23 -0700

    claseviernes04

--- app/Http/Controllers/VariantesCovidController.php
+++ app/Http/Controllers/VariantesCovidController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Variante;
 
 class VariantesCovidController extends Controller
 {
     //
     public function index(){
+        $variantes = Variante::all();
         return view('variantes.index');
+        
     }
 }
